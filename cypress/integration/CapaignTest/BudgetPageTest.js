395498decd14cfbf5a8f9a604b6f68634c4c4ac8
fangming <cjlfm007@gmail.com>
2018-09-21 16:35:30 -0700

    update budget test

--- cypress/integration/CapaignTest/BudgetPageTest.js
+++ cypress/integration/CapaignTest/BudgetPageTest.js
@@ -1,6 +1,6 @@
 describe('Campaign Budget Page Test', function() {
     const random = Math.floor((Math.random() * 10000) + 1);
-    it('Check Budget Page As External User With Single Story', function() {
+    it('Test Budget Page As External User With Single Story', function() {
         cy.loginAsExternal('dev');
         cy.fillExternalForm(random);
         cy.validateNewCampaign(random);
@@ -18,7 +18,7 @@ describe('Campaign Budget Page Test', function() {
         cy.get('#budget-amount-input').type('1000').should('have.value','1000');
         cy.get('#budget-save-button').click();
     })
-    it('Check Budget Page As Admin User With Single Story', function() {
+    it('Test Budget Page As Admin User With Single Story', function() {
         cy.loginAsAdmin('dev');
         cy.fillAdminForm(random);
         cy.validateNewCampaign(random);
@@ -35,8 +35,31 @@ describe('Campaign Budget Page Test', function() {
         cy.get('#budget-amount-input').type('1000').should('have.value','1000');
         cy.get('#budget-save-button').click();
     })
-    it.only('Check Budget Page As Analyst User With Single Story', function() {
+    it('Test Budget Page As Analyst User With Single Story', function() {
         cy.loginAsAnalyst('dev');
         cy.checkAnalystForm();
+    })
+    it.only('Test Budget Page As Analyst User', function() {
+        cy.loginAsAnalyst('dev');
+        cy.url().should('include',"/campaigns");
+        cy.server();
+        cy.get('#search-input').type("test");
+        cy.route('GET','**attributes/saved_report_campaignDashboard').as('getCampaignResults');
+        cy.get('tbody td:nth-child(2)').each((index,$list) => {
+              if(index >= 2){
+                  expect($list).to.contain('test');
+                  cy.wait(['@getCampaignResults']);
+              }
+       });
+       cy.get(':nth-child(1) > :nth-child(2) > app-reportviewer-link.ng-star-inserted > .ng-star-inserted').click();
+       cy.get('#campaign-budget-and-schedule-tab').click();
+       cy.get('#schedule-add-button').should('be.disabled');
+       cy.get('#budget-maxBid-select').should('be.disabled');
+         if(cy.get('#schedule-0') !== null){
+            cy.get('#budget-0-delete-button').should('be.disabled');
+            cy.get('#budget-0-startDate-toggle > .mat-icon-button').should('be.disabled');
+            cy.get('#budget-0-endDate-toggle > .mat-icon-button').should('be.disabled');
+            cy.get('#budget-0-amount-input').should('be.disabled');
+        }
     })
 })
